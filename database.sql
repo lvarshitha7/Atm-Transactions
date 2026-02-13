10dc0e161af393195288db2999c42e9ba40b9e52
Laxmi Varshitha J <varshithajuturu2006@gmail.com>
2026-02-13 08:41:18 +0530

    Created sample users

---
+++ database.sql
@@ -0,0 +1,12 @@
+create database atm_db;
+use atm_db;
+create table users(
+	user_id int primary key,
+    name varchar(40),
+    pin int not null,
+    balance decimal(10,2));
+insert into users values
+(101, "Varshitha" , 1234, 50000);
+select * from users;
+insert into users values
+(102, "Pooja" , 4321 , 5000);
